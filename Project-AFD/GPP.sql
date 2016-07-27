86b5f3e165fc639638d3f168f35edaf370dc4f8a
jingwen <jingwen7.zheng@gmail.com>
2016-07-27 23:07:28 +0200

    Create GPP.sql

---
+++ Project-AFD/GPP.sql
@@ -0,0 +1,17 @@
+-- Remove the empty lines
+DELETE *
+FROM GPP
+WHERE [N° de convention AFD] IS NULL;
+
+-- Determine the primary key
+ALTER TABLE GPP
+ADD PRIMARY KEY ([N° de convention AFD]);
+ 
+-- Put in increasing order
+SELECT *
+FROM GPP
+ORDER BY Pays;
+
+-- update the column "Moyenne des prêts en EUR "
+UPDATE GPP SET [Moyenne des prêts en EUR] = 0
+WHERE [Min des prêts en EUR] = 0 And [Max des prêts en EUR] = 0;
