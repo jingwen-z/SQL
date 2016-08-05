ce7361a0443d08f0b9c730c50518c1b3710072b3
jingwen <jingwen7.zheng@gmail.com>
2016-08-05 17:05:04 +0200

    Update Arrêté Comptable.sql

--- Project-AFD/Arrêté Comptable.sql
+++ Project-AFD/Arrêté Comptable.sql
@@ -6,3 +6,26 @@ WHERE [N° concours] IS NULL;
 -- Remove columns
 ALTER TABLE [Arrêté Comptable]
 DROP COLUMN Clé, [Commentaires DBO au 31/12/2015]
+
+-- Update the column "CNE"
+UPDATE [Arrêté Comptable] SET CNE = ""
+WHERE [CNE GAR] IS NULL
+       OR [CNE DBO] IS NULL;
+
+-- Update the column "RAV sur signé "
+UPDATE [Arrêté Comptable]
+SET [RAV sur signé] = ""
+WHERE [RAV sur signé GAR] IS NULL
+       OR [RAV sur signé DBO] IS NULL;
+
+-- Update the column "RAV non signé "
+UPDATE [Arrêté Comptable]
+SET [RAV non signé] = ""
+WHERE [RAV sur non signé GAR] IS NULL
+       OR [RAV sur non signé DBO] IS NULL;
+
+-- Update the column "Risque"
+UPDATE [Arrêté Comptable]
+SELECT [Risque] = ""
+WHERE [Risque GAR] IS NULL
+       OR [Risque DBO] IS NULL;
