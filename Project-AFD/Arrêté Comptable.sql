0b4bdc7a2c09946c0e33aee65c5c72e7724798c3
jingwen <jingwen7.zheng@gmail.com>
2016-08-22 16:23:55 +0200

    ADD 2 queries - 20160822

--- Project-AFD/Arrêté Comptable.sql
+++ Project-AFD/Arrêté Comptable.sql
@@ -20,26 +20,38 @@ WHERE [N° concours] = "CCM116901"
 ALTER TABLE  [Arrêté Comptable]
 ADD PRIMARY KEY ([N° concours]);
 
--- Update the column "CNE"
+-- Update the column "Ecart-CNE"
 UPDATE [Arrêté Comptable] 
 SET [Ecart-CNE] = ""
 WHERE [CNE GAR] IS NULL
    OR [CNE DBO] IS NULL;
 
--- Update the column "RAV sur signé "
+-- Update the column "Ecart-RAV sur signé "
 UPDATE [Arrêté Comptable]
 SET [Ecart-RAV sur signé] = ""
 WHERE [RAV sur signé GAR] IS NULL
    OR [RAV sur signé DBO] IS NULL;
 
--- Update the column "RAV non signé "
+-- Update the column "Ecart-RAV non signé "
 UPDATE [Arrêté Comptable]
 SET [Ecart-RAV non signé] = ""
 WHERE [RAV sur non signé GAR] IS NULL
    OR [RAV sur non signé DBO] IS NULL;
 
--- Update the column "Risque"
+-- Update the column "Ecart-Risque"
 UPDATE [Arrêté Comptable]
 SET [Ecart-Risque] = ""
 WHERE [Risque GAR] IS NULL
        OR [Risque DBO] IS NULL;
+
+-- Remove the faux values in "Durée du prêt-En mois"
+UPDATE [Arrêté Comptable] SET [Durée du prêt-En mois] = ""
+WHERE [Durée du prêt-En mois] = "15/06/2017"
+       OR [Durée du prêt-En mois] = "30/09/2010"
+       OR [Durée du prêt-En mois] = "30/04/2010"
+       OR [Durée du prêt-En mois] = "28/02/2010";
+
+-- Correct a value in "TA transmis à DBO"
+UPDATE [Arrêté Comptable]
+SET [TA transmis à DBO] = "16/09/2013"
+WHERE [TA transmis à DBO] = "30/04/2013-16/09/2013";
