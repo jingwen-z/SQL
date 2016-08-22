88ab3e47873977db6356c07b61d20137e797f326
jingwen <jingwen7.zheng@gmail.com>
2016-08-22 16:13:13 +0200

    ADD group name "Ecart"

--- Project-AFD/Arrêté Comptable.sql
+++ Project-AFD/Arrêté Comptable.sql
@@ -22,24 +22,24 @@ ADD PRIMARY KEY ([N° concours]);
 
 -- Update the column "CNE"
 UPDATE [Arrêté Comptable] 
-SET CNE = ""
+SET [Ecart-CNE] = ""
 WHERE [CNE GAR] IS NULL
-       OR [CNE DBO] IS NULL;
+   OR [CNE DBO] IS NULL;
 
 -- Update the column "RAV sur signé "
 UPDATE [Arrêté Comptable]
-SET [RAV sur signé] = ""
+SET [Ecart-RAV sur signé] = ""
 WHERE [RAV sur signé GAR] IS NULL
-       OR [RAV sur signé DBO] IS NULL;
+   OR [RAV sur signé DBO] IS NULL;
 
 -- Update the column "RAV non signé "
 UPDATE [Arrêté Comptable]
-SET [RAV non signé] = ""
+SET [Ecart-RAV non signé] = ""
 WHERE [RAV sur non signé GAR] IS NULL
-       OR [RAV sur non signé DBO] IS NULL;
+   OR [RAV sur non signé DBO] IS NULL;
 
 -- Update the column "Risque"
 UPDATE [Arrêté Comptable]
-SELECT [Risque] = ""
+SELECT [Ecart-Risque] = ""
 WHERE [Risque GAR] IS NULL
        OR [Risque DBO] IS NULL;
