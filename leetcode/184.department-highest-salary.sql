a2d27365c816e37fc75ad2a24aa2f777a0546d09
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2019-11-28 16:40:25 +0100

    add question

--- leetcode/184.department-highest-salary.sql
+++ leetcode/184.department-highest-salary.sql
@@ -1,10 +1,48 @@
 -- Medium
-SELECT Department.Name AS "Department", Employee.Name AS "Employee", Employee.Salary AS Salary
+
+-- The Employee table holds all employees. Every employee has an Id, a salary,
+-- and there is also a column for the department Id.
+--
+--+----+-------+--------+--------------+
+--| Id | Name  | Salary | DepartmentId |
+--+----+-------+--------+--------------+
+--| 1  | Joe   | 70000  | 1            |
+--| 2  | Jim   | 90000  | 1            |
+--| 3  | Henry | 80000  | 2            |
+--| 4  | Sam   | 60000  | 2            |
+--| 5  | Max   | 90000  | 1            |
+--+----+-------+--------+--------------+
+-- The Department table holds all departments of the company.
+--
+--+----+----------+
+--| Id | Name     |
+--+----+----------+
+--| 1  | IT       |
+--| 2  | Sales    |
+--+----+----------+
+-- Write a SQL query to find employees who have the highest salary in each of
+-- the departments. For the above tables, your SQL query should return the
+-- following rows (order of rows does not matter).
+--
+--+------------+----------+--------+
+--| Department | Employee | Salary |
+--+------------+----------+--------+
+--| IT         | Max      | 90000  |
+--| IT         | Jim      | 90000  |
+--| Sales      | Henry    | 80000  |
+--+------------+----------+--------+
+
+SELECT
+    Department.Name AS "Department"
+    , Employee.Name AS "Employee"
+    , Employee.Salary AS Salary
 FROM Employee
 INNER JOIN Department
 ON Employee.DepartmentId = Department.Id
 WHERE (Employee.DepartmentId, Employee.Salary) IN (
-    SELECT Employee.DepartmentId, MAX(Salary)
+    SELECT
+        Employee.DepartmentId
+        , MAX(Salary)
     FROM Employee
     GROUP BY Employee.DepartmentId
 )
