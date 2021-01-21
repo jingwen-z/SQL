9aa72a4821cf29ac02edb9836fe2a0bb5c6f0d66
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2021-01-21 10:57:37 +0100

    employee names and salaries

--- HackerRank/Employee Names.sql
+++ HackerRank/Employee Names.sql
@@ -1,12 +1,8 @@
 -- Easy
 
--- Query the Name of any student in STUDENTS who scored higher than Marks.
--- Order your output by the last three characters of each name.
--- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.),
--- secondary sort them by ascending ID.
--- https://www.hackerrank.com/challenges/more-than-75-marks/
+-- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+-- https://www.hackerrank.com/challenges/name-of-employees/
 
-SELECT Name
-FROM STUDENTS
-WHERE Marks > 75
-ORDER BY RIGHT(Name, 3), ID
+SELECT name
+FROM Employee
+ORDER BY name
--- HackerRank/Employee Salaries.sql
+++ HackerRank/Employee Salaries.sql
@@ -1,8 +1,11 @@
 -- Easy
 
--- Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
--- https://www.hackerrank.com/challenges/name-of-employees/
+-- Write a query that prints a list of employee names (i.e.: the name attribute) for employees in
+-- Employee having a salary greater than per month who have been employees for less than months. Sort your result by ascending employee_id.
+-- https://www.hackerrank.com/challenges/salary-of-employees/
 
 SELECT name
 FROM Employee
-ORDER BY name
+WHERE salary > 2000
+AND months < 10
+ORDER BY employee_id
