8d0c107563be4d7a82bf15cb7ac8bdf0db8e0eb6
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2022-01-11 22:45:56 +0100

    Weather Observation Station 5

--- HackerRank/Weather Observation Station 5.sql
+++ HackerRank/Weather Observation Station 5.sql
@@ -1,9 +1,27 @@
 -- Easy
 
--- Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
--- https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?h_r=next-challenge&h_v=zen
+-- Query the two cities in STATION with the shortest and longest CITY names, as well as their respective lengths (i.e.: number of characters in the name).
+-- If there is more than one smallest or largest city, choose the one that comes first when ordered alphabetically.
 
-SELECT NAME
-FROM CITY
-WHERE COUNTRYCODE = 'USA'
-AND POPULATION > 120000
+-- Sample Input
+-- For example, CITY has four entries: DEF, ABC, PQRS and WXY.
+
+-- Sample Output
+-- ABC 3
+-- PQRS 4
+
+-- https://www.hackerrank.com/challenges/weather-observation-station-5/problem
+
+(
+SELECT CITY, LENGTH(CITY) AS LEN_CITY
+FROM STATION
+ORDER BY LEN_CITY, CITY
+LIMIT 1
+)
+UNION
+(
+SELECT CITY, LENGTH(CITY) AS LEN_CITY
+FROM STATION
+ORDER BY LEN_CITY DESC, CITY
+LIMIT 1
+)
