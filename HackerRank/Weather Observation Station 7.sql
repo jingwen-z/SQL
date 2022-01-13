d749ace93409e3b073e3ebb5976202f4228a5f2c
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2022-01-13 11:13:14 +0100

    Weather Observation Station 7

--- HackerRank/Weather Observation Station 7.sql
+++ HackerRank/Weather Observation Station 7.sql
@@ -1,12 +1,12 @@
 -- Easy
 
--- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
--- https://www.hackerrank.com/challenges/weather-observation-station-6/problem?h_r=next-challenge&h_v=zen
+-- Query the list of CITY names ending with vowels (a, e, i, o, u) from STATION. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-7/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
 SELECT DISTINCT CITY
 FROM STATION
-WHERE CITY LIKE 'a%'
-OR CITY LIKE 'e%'
-OR CITY LIKE 'i%'
-OR CITY LIKE 'o%'
-OR CITY LIKE 'u%'
+WHERE CITY LIKE '%a'
+OR CITY LIKE '%e'
+OR CITY LIKE '%i'
+OR CITY LIKE '%o'
+OR CITY LIKE '%u'
