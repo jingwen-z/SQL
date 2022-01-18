44f9f4c115d390031213e22fe37e952d4ace3453
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2022-01-18 23:06:28 +0100

    Weather Observation Station 11

--- HackerRank/Weather Observation Station 11.sql
+++ HackerRank/Weather Observation Station 11.sql
@@ -1,8 +1,9 @@
 -- Easy
 
--- Query the list of CITY names from STATION that do not end with vowels. Your result cannot contain duplicates.
--- https://www.hackerrank.com/challenges/weather-observation-station-10/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+-- https://www.hackerrank.com/challenges/weather-observation-station-11/
 
 SELECT DISTINCT CITY
 FROM STATION
-WHERE RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+OR RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
