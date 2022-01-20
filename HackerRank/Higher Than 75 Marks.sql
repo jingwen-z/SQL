aaab493770a800f4233c82625d1e3efaf5f74620
Jingwen ZHENG <jingwen7.zheng@gmail.com>
2022-01-20 10:56:53 +0100

    Higher than 75 marks

--- HackerRank/Higher Than 75 Marks.sql
+++ HackerRank/Higher Than 75 Marks.sql
@@ -1,9 +1,12 @@
 -- Easy
 
--- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels. Your result cannot contain duplicates.
--- https://www.hackerrank.com/challenges/weather-observation-station-12/
+-- Query the Name of any student in STUDENTS who scored higher than Marks.
+-- Order your output by the last three characters of each name.
+-- If two or more students both have names ending in the same last three characters (i.e.: Bobby, Robby, etc.),
+-- secondary sort them by ascending ID.
+-- https://www.hackerrank.com/challenges/more-than-75-marks/
 
-SELECT DISTINCT CITY
-FROM STATION
-WHERE LEFT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
-AND RIGHT(CITY, 1) NOT IN ('a', 'e', 'i', 'o', 'u')
+SELECT Name
+FROM STUDENTS
+WHERE Marks > 75
+ORDER BY RIGHT(Name, 3), ID
